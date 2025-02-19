61211f42bedf0e1767988aef1200ef1c98d26d2a
wjia <wjia@microsoft.com>
2025-02-19 12:47:15 +0800

    add use case for experiment

---
+++ examples/use-cases/check_my_etree.py
@@ -0,0 +1,100 @@
+import os
+
+from langchain_openai import ChatOpenAI
+from pydantic import SecretStr
+from browser_use import Agent, Browser, BrowserConfig
+from browser_use.browser.browser import BrowserContextConfig
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import asyncio
+
+task="""
+   ### Prompt for Testing Agent - Check My ETree level, daily tasks, and certificates
+
+**Objective:**  
+Visit [Wallet Homepage](edge://wallet), open ETree widget, check current level, today's tasks, and certificates.
+
+**Important:**
+- Make sure that you accurately check the elements I specified on the page.
+- If you open any 2nd-level window of ETree widget, you can go back by clicking the '<' button of the top left. This will help you navigate easier.
+---
+
+### Step 1: Navigate to the Wallet Homepage
+- Open [Wallet Homepage](edge://wallet). You can input 'edge://wallet' in the address bar, then hit enter. Noted that, this is an internal page, please stop and ask me if you found the page cannot be loaded.
+- Make sure the page was fully loaded.
+---
+
+### Step 2: Open the ETree widget
+- There's an floating bubble showing a small tree on the bottom-left.
+- Check what the current level is, it usually shown like 'Level N' in white color on the bottom of the bubble.
+- Click this floating bubble to open the ETree pop-up window.
+---
+
+### Step 3: Find out today's tasks
+- On the bottom of the pop-up window, there's a section shows the Today's tasks, which is designed to help me to promote my ETree level and earn the rewards.
+- Check each task, including the task name and the reward. 
+---
+
+### Step 4: Check my current certificates
+- On the top-left of the pop-up window, there'a certificate icon.
+- Click this icon to open the 'My Certificates' page, it shows all my badges.
+- Check each badge I owned, including the badge's name and current progress.
+---
+
+### Step 5: Gather all information and let me know
+- Output all the informations I ask you to check above as following format:
+  - **My current ETree Level is:**.
+  - **My today's tasks are:**, ** (including the name and reward).
+  - **My certificates are:**, ** (including the name and progress).
+
+**Important:** Ensure efficiency and accuracy throughout the process.
+"""
+
+initial_actions = [
+    # switch to wallet page if internal page cannot be loaded
+	{'switch_tab': {'page_id': '0'}},
+]
+
+browser = Browser(
+	config=BrowserConfig(
+		headless=False,
+		disable_security=True,
+		chrome_instance_path='C:\\Users\\wjia\\AppData\\Local\\Microsoft\\Edge SxS\\Application\\msedge.exe',
+		extra_chromium_args=['--profile-directory=Profile 1'] + ['--user-data-dir=C:\\Users\\wjia\\AppData\\Local\\Microsoft\\Edge SxS\\User Data\\'],
+        # new_context_config=BrowserContextConfig(
+        #     trace_path='./tmp/check_my_etree_trace',
+        #     save_recording_path='./tmp/check_my_etree_recording',
+        #     no_viewport=False,
+		# 	browser_window_size={
+		# 		'width': 1280,
+		# 		'height': 1100,
+		# 	},
+		# ),
+    )
+)
+
+agent = Agent(
+    task=task,
+    initial_actions=initial_actions,
+    llm=ChatOpenAI(model="gpt-4o"),
+    use_vision=True,
+    browser=browser,
+    max_actions_per_step=10,
+    )
+
+async def main():
+    history = await agent.run(max_steps=50)
+    history_file = os.path.join('.\\tmp\\check_my_etree_history\\', f"{agent.agent_id}.json")
+    agent.save_history(history_file)
+    result = history.final_result()
+    if result:
+        print(f'{result}')
+    else:
+        print('No result')
+
+    await browser.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ examples/use-cases/gen_test_case.py
@@ -0,0 +1,84 @@
+import openai
+import json
+import os
+
+from typing import Dict, Any
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class TestCaseGenerator:
+    def __init__(self, model: str = "gpt-4o-mini"):
+        self.api_key = os.getenv("OPENAI_API_KEY")
+        if not self.api_key:
+            raise ValueError("OpenAI API key is required, please set OPENAI_API_KEY in environment variable or dotenv file")
+        self.model = model
+        self.client = openai.OpenAI(api_key=self.api_key)
+        self.messages = []
+
+    def generate_test_case(self, use_case_description: str) -> Dict[str, Any]:
+        # Add system prompt
+        system_prompt = """
+        You are a product expert and testing expert.
+        You are good at designing test cases from a given use case description or product feature description.
+        I'm building a Web Agent by leveraging LLM and other frameworks like browser-use, it can help me to test functionality of a web product.
+        You'll be given a use case description or product feature description and you need to generate test cases as following JSON format.
+        ```
+        [
+            {
+                "test_case_name": "a brief test case name",
+                "test_case_description": "test case description",
+                "steps": [
+                    {
+                        "step_name": "a brief step name",
+                        "step_description": "step description, like how to do, what to see, what to click, what to input, etc.",
+                        "expected_result": "expected result if has"
+                    }
+                ]
+            }
+        ]
+        ```
+        Other hints may be usefule are:
+        - All use case descriptions or product feature descriptions are written in natural language, either in English or Simplified Chinese.
+        - All test steps should be able to act on a web page.
+        - Carefully consider how to design the test steps according to each functionality.
+        - Carefully consider edge cases and corner cases
+        - Also consider the behavior or interaction on the web page.
+        """
+
+        # Add user prompt
+        user_prompt = f"""
+        Use case description:
+        {use_case_description}
+        """
+        self.messages.append({"role": "system", "content": system_prompt})
+        self.messages.append({"role": "user", "content": user_prompt})
+
+        try:
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=self.messages,
+                response_format={"type": "json_object"}
+            )
+            return json.loads(response.choices[0].message.content)
+        except Exception as e:
+            return {
+                "error": str(e)
+            }
+
+def main():
+    test_case_generator = TestCaseGenerator()
+    test_cases = test_case_generator.generate_test_case("""
+- 该功能名为电子树（ETree），它是虚拟的植树活动，鼓励用户完成一些任务，提升等级，并且获得对应的证书或徽章。
+- 该功能的入口位于 edge://wallet 页面左下角，它是一个悬浮的小组件，组件中会显示一棵树，同时下方会展示用户当前的等级（Level）。
+- 点击该悬浮组件，会打开一个窗口，是该功能的主要界面。里面包含：
+- 页面在左上角显示用户的昵称
+- 页面背景有一幅绿意盎然的图片以及一棵小树苗
+- 页面下方则展示了用户的每日任务（today's tasks），任务有简单的描述信息以及对应的奖励
+- 页面左上方用户昵称下有一个奖章图标，点击该图标将打开证书（certificates）页面，里面将展示用户当前获得的证书或奖章，包含名称，以及当前进度。
+- 从证书页面可以返回到主页面
+    """)
+    print(json.dumps(test_cases, indent=2))
+
+if __name__ == "__main__":
+    main()
